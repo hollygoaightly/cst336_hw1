5630f114af1e70ac99cd95d5580bdcbfa6150bd9
EC2 Default User <ec2-user@ip-172-31-17-249.us-west-1.compute.internal>
2020-11-04 06:03:51 +0000

    finished

---
+++ home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta char="utf-8"/>
+        <title>Women in Computer Science</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>Women in Computer Science</h1>
+        </header>
+        <hr />
+        <nav>
+            <a href="home.php"><span id="current">Home</span></a>
+            <a href="ada.html">Ada Lovelace</a>
+            <a href="bombe.html">Bombe</a>
+            <a href="grace.html">Grace Hopper</a>
+        </nav>
+        <br /><br />
+        <main>
+            <figure>
+                <img src="img/homepage.png">
+            </figure>
+            <div>
+                <p>Since as early as he 19th century, women have been involved in what has evolved into the computer science field today.</p>
+                <p>Both our first algorithm intended for a computer <i>and</i> our first compiler were designed by women.</p>
+                <p>Despite their involvement in the field from its very origin, as with many other fields, many of these women and their achievements are not widely known.</p>
+                <p>This web page provides a background of some of these women and their achievements.</p>
+                <p>Please click on the various tabs to learn a bit more!</p>
+                <br /><br />
+            </div>
+        </main>
+       <footer>
+            <hr>
+            CST336 Internet Programming. 2020&copy Stephens <br />
+            <div>
+                References<br />
+                <ol>
+                    <li><a href="https://en.wikipedia.org/wiki/Women_in_computing">Women in computing</a></li>
+                    <li><a href="https://en.wikipedia.org/wiki/Ada_Lovelace">Ada Lovelace</a></li>
+                    <li><a href="https://en.wikipedia.org/wiki/Bombe">Bombe</a></li>
+                    <li><a href="https://en.wikipedia.org/wiki/Grace_Hopper">Grace Hopper</a></li>
+                </ol>
+            </div>
+        </footer>
+    </body>
+</html>
